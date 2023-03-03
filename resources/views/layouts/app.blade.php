db7f577e9a93b561f771dd78c8079fb7976501e0
phptech0000 <php.tech0000@outlook.com>
2023-03-03 11:00:58 +0100

    Instaling and configuring tailwindcss to the project

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,14 +5,14 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Devstagram: @yield('titulo')</title>        
-        
+        @vite('resources/css/app.css')
     </head>
     <body>
         <nav>
             <a href="/">Principal</a>
             <a href="/nosotros">Nosotros</a>
         </nav>
-        <h1>@yield('titulo')</h1>
+        <h1 class="text-4xl">@yield('titulo')</h1>
         <hr>
         @yield('contenido')
     </body>
