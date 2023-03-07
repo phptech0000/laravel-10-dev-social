3221b78adc67a64e5307df22ac53f5e328333c2d
phptech0000 <php.tech0000@outlook.com>
2023-03-07 13:13:49 +0100

    Completando configuracion de tailwind y vite para laravel

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,13 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+        watch: {
+            usePolling: true
+        }
+    },
 });
