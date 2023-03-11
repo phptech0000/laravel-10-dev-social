1d38f3622ae9b2c5399cabb23c34f9b9d3cb048b
phptech0000 <php.tech0000@outlook.com>
2023-03-11 02:49:42 +0100

    Defining the main layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,5 +20,14 @@
             </nav>
         </div>
        </header>
+       <main class="container mx-auto mt-10">
+            <h2 class="font-black text-center text-3xl mb-10">
+                @yield('titulo')
+            </h2>
+            @yield('contenido')
+       </main>
+       <footer class="text-center p-5 text-gray-500 font-bold uppercase">
+            DevsTagram - Todos los derechos reservados {{date('Y')}}
+       </footer>
     </body>
 </html>
