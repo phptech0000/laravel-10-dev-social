b2c8705d32e33460b55a04e1099693f560cc8ced
phptech0000 <php.tech0000@outlook.com>
2023-03-12 15:48:14 +0100

    Creating registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,3 +3,46 @@
 @section('titulo')
 Registrate en DevsTagram
 @endsection
+
+@section('contenido')
+    <div class="md:flex md:justify-center md:gap-10 md:items-center ">
+        <div class="md:w-6/12 p-5">
+            <img src="{{ asset('img/registrar.jpg')}}" alt="Imagen regsitro de usuarios" >
+        </div>
+        <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl">
+            <form>
+                <div class="mb-5">
+                    <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Nombre
+                    </label>
+                    <input id="name" name="name" type="text" placeholder="Tu nombre" class="border p-3 w-full rounded-lg">
+                </div>
+                <div class="mb-5">
+                    <label for="username" class="mb-2 block uppercase text-gray-500 font-bold">
+                        UserName
+                    </label>
+                    <input id="username" name="username" type="text" placeholder="Tu nombre de usuario" class="border p-3 w-full rounded-lg">
+                </div>
+                <div class="mb-5">
+                    <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Email
+                    </label>
+                    <input id="email" name="email" type="email" placeholder="Tu email de registro" class="border p-3 w-full rounded-lg">
+                </div>
+                <div class="mb-5">
+                    <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Password
+                    </label>
+                    <input id="password" name="password" type="password" placeholder="Password de registro" class="border p-3 w-full rounded-lg">
+                </div>
+                <div class="mb-5">
+                    <label for="password_confirmation" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Repetir Password
+                    </label>
+                    <input id="password_confirmation" name="password_confirmation" type="password" placeholder="Repite tu password" class="border p-3 w-full rounded-lg">
+                </div>
+                <input type="submit" value="Crear Cuenta" class="bg-sky-600  hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
+            </form>
+        </div>
+    </div>
+@endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,13 +20,13 @@
             </nav>
         </div>
        </header>
-       <main class="container mx-auto mt-10">
+       <main class="container mx-auto mt-10 p-5">
             <h2 class="font-black text-center text-3xl mb-10">
                 @yield('titulo')
             </h2>
             @yield('contenido')
        </main>
-       <footer class="text-center p-5 text-gray-500 font-bold uppercase">
+       <footer class="text-center p-5 text-gray-500 font-bold uppercase mt-10">
             DevsTagram - Todos los derechos reservados {{date('Y')}}
        </footer>
     </body>
