1ec9ebd01d42963f294662466c57bea4fbe1bd41
phptech0000 <php.tech0000@outlook.com>
2023-03-12 16:06:52 +0100

    Adding csrf security token to the registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,6 +11,7 @@ Registrate en DevsTagram
         </div>
         <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl">
             <form action="/crear-cuenta" method="POST">
+                @csrf
                 <div class="mb-5">
                     <label for="name" class="mb-2 block uppercase text-gray-500 font-bold">
                         Nombre
