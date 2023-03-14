5631d7e9070d3a1aa413c375f86116c345f1388a
phptech0000 <php.tech0000@outlook.com>
2023-03-14 16:01:30 +0100

    creating route names

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,5 @@ use App\Http\Controllers\RegisterController;
 Route::get('/', function () {
     return view('principal');
 });
-Route::get('/crear-cuenta', [RegisterController::class,'index']);
-Route::post('/crear-cuenta', [RegisterController::class,'store']);
+Route::get('/register', [RegisterController::class,'index'])->name('register');
+Route::post('/register', [RegisterController::class,'store']);
