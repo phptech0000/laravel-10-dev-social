0c2c7dcc8d29b0856a4fa46bd0b0ef4daa70fee4
phptech0000 <php.tech0000@outlook.com>
2023-03-15 15:01:35 +0100

    Validating password confirmation and adding column username using migrations

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -12,13 +12,12 @@ class RegisterController extends Controller
     }
     public function store(Request $request)
     {
-        //dd($request);
-        //dd($request->get("email"));
+        
         $this->validate($request,[
             "name"=>'required|max:30',
             "username"=>'required|unique:users|min:3|max:20',
             "email"=>'required|email|unique:users|max:60',
-            "password"=>'required'
+            "password"=>'required|confirmed|min:6'
         ]);
     }
 }
