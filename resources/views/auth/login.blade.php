10c6c05521f3125ac2fd5254db3389816585364b
phptech0000 <php.tech0000@outlook.com>
2023-03-26 03:05:14 +0200

    protecting dashboard with middleware and creating login route, controller and view

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,53 @@
+@extends('layouts.app')
+
+@section('titulo')
+Login to DevsTagram
+@endsection
+
+@section('contenido')
+    <div class="md:flex md:justify-center md:gap-10 md:items-center ">
+        <div class="md:w-6/12 p-5">
+            <img src="{{ asset('img/login.jpg')}}" alt="Image user login" >
+        </div>
+        <div class="md:w-4/12 bg-white p-6 rounded-lg shadow-xl">
+            <form novalidate>
+                @csrf               
+                
+                <div class="mb-5">
+                    <label for="email" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Email
+                    </label>
+                    <input 
+                        id="email" 
+                        name="email" 
+                        type="email" 
+                        placeholder="Your registration email" 
+                        class="border p-3 w-full rounded-lg @error('email') border-red-500 @enderror"
+                        value={{ old('email') }}
+                    >
+                    @error("email")
+                        <p class=" bg-red-500 text-white my-0 rounded-md p-2 text-sm text-center">{{$message}}</p>                        
+                     @enderror
+                </div>
+                <div class="mb-5">
+                    <label for="password" class="mb-2 block uppercase text-gray-500 font-bold">
+                        Password
+                    </label>
+                    <input 
+                        id="password" 
+                        name="password" 
+                        type="password" 
+                        placeholder="Your registration password" 
+                        class="border p-3 w-full rounded-lg @error('password') border-red-500 @enderror"
+                        value={{ old('email') }}
+                    >
+                    @error("password")
+                        <p class=" bg-red-500 text-white my-0 rounded-md p-2 text-sm text-center">{{$message}}</p>                        
+                     @enderror
+                </div>
+                
+                <input type="submit" value="Login" class="bg-sky-600  hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg">
+            </form>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
@@ -20,5 +21,6 @@ Route::get('/', function () {
 });
 Route::get('/register', [RegisterController::class,'index'])->name('register');
 Route::post('/register', [RegisterController::class,'store']);
+Route::get('/login',[LoginController::class,'index'])->name('login');
 
 Route::get('/muro',[PostController::class,'index'])->name("post.index");
