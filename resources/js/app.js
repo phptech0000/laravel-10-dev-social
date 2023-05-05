e7f03bbc52677b651f1eea188ff3d346bf9c071e
phptech0000 <php.tech0000@outlook.com>
2023-05-05 22:10:59 +0200

    Setting dropzone events

--- resources/js/app.js
+++ resources/js/app.js
@@ -10,3 +10,19 @@ const dropzone= new Dropzone('#dropzone',{
     maxFiles: 1,
     uploadMultiple: false,
 })
+
+dropzone.on("sending",function(file, xhr,formData){
+    console.log(formData);
+})
+
+dropzone.on("success",function(file, response){
+    console.log(response);
+})
+
+dropzone.on("error",function(file,message){
+    console.log(message)
+})
+
+dropzone.on("removedfile",function()){
+    console.log("file removed");
+}
