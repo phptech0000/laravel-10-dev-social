5a2d238f62e0258066d4ca51778ca12d7a78e4c8
phptech0000 <php.tech0000@outlook.com>
2023-05-11 16:51:51 +0200

    Adding columns for migration of Posts table

--- database/migrations/2023_05_10_015758_create_posts_table.php
+++ database/migrations/2023_05_10_015758_create_posts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string("titulo");
+            $table->text("descripcion");
+            $table->string("imagen");
+            $table->foreignId("user_id")->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
