593d448349281cb15940181a7174f0fb6823c2fe
phptech0000 <php.tech0000@outlook.com>
2023-05-22 03:54:20 +0200

    creating post - user relation

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -14,4 +14,9 @@ class Post extends Model
         'image',
         'user_id'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class)->select(['name','username']);
+    }
 }
