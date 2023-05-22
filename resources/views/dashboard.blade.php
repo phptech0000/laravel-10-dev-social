b4d87201acfcc968e8373bac56e80f8ea306e0f7
phptech0000 <php.tech0000@outlook.com>
2023-05-22 23:32:19 +0200

    Showing posts on user profile dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -25,4 +25,22 @@
             </div>
         </div>
     </div>
+    <section class="container mx-auto mt-10">
+        <h2 class="text-4xl text-center font-black my-10">Posts</h2>
+
+        @if ($posts->count())
+        
+            <div class="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
+            @foreach ($posts as $post)
+                <div>
+                    <a href="">
+                        <img src="{{asset('uploads').'/'.$post->image}}" alt="Post image: {{$post->title}}">
+                    </a>
+                </div>            
+            @endforeach
+            </div>
+        @else
+            <p class="text-gray-600 uppercase text-sm text-center font-bold">No posts were found</p>        
+        @endif
+    </section>
 @endsection
